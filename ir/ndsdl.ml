1a1074d8f745b0eb1c2c3d24d12de6d192c470a2
Samuel Kim <ubuntor2000@gmail.com>
2019-11-27 01:05:21 -0500

    fix ir

--- ir/ndsdl.ml
+++ ir/ndsdl.ml
@@ -13,42 +13,65 @@ module Term = struct
   [@@deriving sexp]
 end
 
-(* hack for separate modules with mutually recursive types *)
-module rec Outer : sig
-  module Program : sig
-    type t =
-      | Assign of string * Term.t
-      | Assignany of string
-      | Test of Outer.Formula.t
-      | Compose of t * t
-      | Loop of t * t
-      | Choice of t * t
-      | Probchoice of (int * t) * (int * t) (* TODO *)
-    [@@deriving sexp]
-  end
+(* duplicate types needed for mutually recursive types in separate modules and the [@@deriving sexp] *)
+module rec Program : sig
+  type t =
+    | Assign of string * Term.t
+    | Assignany of string
+    | Test of Formula.t
+    | Compose of t * t
+    | Loop of t * t
+    | Choice of t * t
+    | Probchoice of (int * t) * (int * t) (* TODO *)
+  [@@deriving sexp]
+end = struct
+  type t =
+    | Assign of string * Term.t
+    | Assignany of string
+    | Test of Formula.t
+    | Compose of t * t
+    | Loop of t * t
+    | Choice of t * t
+    | Probchoice of (int * t) * (int * t) (* TODO *)
+  [@@deriving sexp]
+end
 
-  module Formula : sig
-    type t =
-      | True
-      | False
-      | Or of t * t
-      | Not of t * t
-      | And of t * t
-      | Implies of t * t
-      | Equiv of t * t
-      | Eq of Term.t * Term.t
-      | Lt of Term.t * Term.t
-      | Le of Term.t * Term.t
-      | Gt of Term.t * Term.t
-      | Ge of Term.t * Term.t
-      | Forall of string * t
-      | Exists of string * t
-      | Box of Outer.Program.t * t
-      | Diamond of Outer.Program.t * t
-    [@@deriving sexp]
-  end
-end =
-  Outer
-
-module Program = Outer.Program
-module Formula = Outer.Formula
+and Formula : sig
+  type t =
+    | True
+    | False
+    | Or of t * t
+    | Not of t * t
+    | And of t * t
+    | Implies of t * t
+    | Equiv of t * t
+    | Eq of Term.t * Term.t
+    | Lt of Term.t * Term.t
+    | Le of Term.t * Term.t
+    | Gt of Term.t * Term.t
+    | Ge of Term.t * Term.t
+    | Forall of string * t
+    | Exists of string * t
+    | Box of Program.t * t
+    | Diamond of Program.t * t
+  [@@deriving sexp]
+end = struct
+  type t =
+    | True
+    | False
+    | Or of t * t
+    | Not of t * t
+    | And of t * t
+    | Implies of t * t
+    | Equiv of t * t
+    | Eq of Term.t * Term.t
+    | Lt of Term.t * Term.t
+    | Le of Term.t * Term.t
+    | Gt of Term.t * Term.t
+    | Ge of Term.t * Term.t
+    | Forall of string * t
+    | Exists of string * t
+    | Box of Program.t * t
+    | Diamond of Program.t * t
+  [@@deriving sexp]
+end
